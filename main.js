be8a24de41346ee6948d0cbdd7e6a9ba8bd417f6
alisa-val <57110353+alisa-val@users.noreply.github.com>
2020-11-01 20:15:03 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    let dropdowns = document.getElementsByClassName("dropdown-content");
+    let i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
